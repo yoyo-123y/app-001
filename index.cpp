be42a670399b78f5fab86389baac4b008e54133e
youssif roushdy <youssifalsayad5656@gmail.com>
2025-07-24 00:39:28 +0300

    created a project using git

---
+++ index.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+bool checkPalindrome(string word){
+    int string_len = word.length();
+    bool flag = true;
+    
+    for(int i = 0; i < (string_len/2); i++){
+        if(word[i] != word[ string_len - i-1]){
+            flag = false;
+            break;
+        }
+
+    }
+    return flag;
+}
+int main(){
+    cout << "Enter the word = ";
+    string word;
+    cin >> word;
+    cout << endl;
+    bool result = checkPalindrome(word);
+    result?cout<<"it is palindrome":cout<<"it is not palindrome";
+    cin.ignore();
+    cout << "Press Enter to exit...";
+    cin.get(); // Waits for Enter
+    return 0;
+}
